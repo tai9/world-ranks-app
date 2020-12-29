ce4eaa1ade084518188f6f31bd8fbdd8b0cc85ac
Tan Tai <tantai.it95@gmail.com>
2020-12-29 17:20:02 +0700

    create countries table and sort data by name or population

---
+++ src/components/CountriesTable/CountriesTable.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import styles from "./Countries.module.css";
+import KeyboardArrowDownRoundedIcon from "@material-ui/icons/KeyboardArrowDownRounded";
+import KeyboardArrowUpRoundedIcon from "@material-ui/icons/KeyboardArrowUpRounded";
+import { type } from "os";
+
+type Props = {
+  countries?: CountryProps[];
+};
+
+type CountryProps = {
+  name: string;
+  population: number;
+};
+
+type DirectionType = "asc" | "desc";
+type CountryType = "name" | "population";
+
+type SortArrowProps = { sort: DirectionType };
+
+const orderBy = (
+  countries: CountryProps[],
+  value: CountryType,
+  direction: DirectionType
+) => {
+  return direction === "asc"
+    ? [...countries].sort((a, b: CountryProps) =>
+        a[value] > b[value] ? 1 : -1
+      )
+    : [...countries].sort((a, b: CountryProps) =>
+        a[value] > b[value] ? -1 : 1
+      );
+};
+
+const SortArrow = ({ sort }: SortArrowProps) => {
+  return sort === "desc" ? (
+    <div className={styles.heading_arrow}>
+      <KeyboardArrowDownRoundedIcon color="inherit" />
+    </div>
+  ) : (
+    <div className={styles.heading_arrow}>
+      <KeyboardArrowUpRoundedIcon color="inherit" />
+    </div>
+  );
+};
+
+const CountriesTable: React.FC<Props> = ({ countries }) => {
+  const [direction, setDirection] = useState<DirectionType>("desc");
+  const [valueDirection, setValueDirection] = useState<CountryType>("name");
+  const orderdCountries = orderBy(countries, valueDirection, direction);
+
+  const switchDirection = () => {
+    direction === "desc" ? setDirection("asc") : setDirection("desc");
+  };
+
+  const setValueAndDirection = (value: CountryType) => {
+    setValueDirection(value);
+    switchDirection();
+  };
+
+  return (
+    <div>
+      <div className={styles.heading}>
+        <button
+          className={styles.heading_name}
+          onClick={() => setValueAndDirection("name")}
+        >
+          <div>Name</div>
+          <SortArrow sort={direction} />
+        </button>
+        <button
+          className={styles.heading_population}
+          onClick={() => setValueAndDirection("population")}
+        >
+          <div>Population</div>
+          <SortArrow sort={direction} />
+        </button>
+      </div>
+
+      {orderdCountries.map((country: CountryProps, index) => {
+        return (
+          <div className={styles.row} key={index}>
+            <div className={styles.name}>{country.name}</div>
+            <div className={styles.population}>{country.population}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CountriesTable;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { GetStaticProps } from "next";
 import React from "react";
+import CountriesTable from "../components/CountriesTable/CountriesTable";
 import Layout from "../components/Layout/Layout";
-import SearchInput from "../components/Layout/SearchInput/SearchInput";
+import SearchInput from "../components/SearchInput/SearchInput";
 import styles from "../styles/Home.module.css";
 
 type Props = {
@@ -9,13 +10,13 @@ type Props = {
 };
 
 const Home: React.FC<Props> = ({ countries }) => {
-  console.log(countries);
-
   return (
     <Layout title="World Ranks">
       <div className={styles.counts}>Found {countries.length} countries</div>
 
       <SearchInput placeholder="Filter by Name, Region or SubRegion" />
+
+      <CountriesTable countries={countries} />
     </Layout>
   );
 };
